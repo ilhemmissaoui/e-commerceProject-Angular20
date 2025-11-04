0ef81e2faad73524d27baf37d06c17ab0c311096
ilhemmissaoui <ilhem.missaoui22@gmail.com>
2025-11-04 17:49:09 +0100

    First commit

---
+++ src/stores/cart/cart.actions.ts
@@ -0,0 +1,12 @@
+import { Product } from '@/types/product.type';
+import { createAction, props } from '@ngrx/store';
+
+export const addToCart = createAction(
+  '[Cart] Add to Cart',
+  props<{ product: Product }>()
+);
+
+export const removeFromCart = createAction(
+  '[Cart] Remove from Cart',
+  props<{ productId: string }>()
+);
